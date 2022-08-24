ee8b911d821f7dbb0bbf0ea8b4d339526f7a9619
Jose Carrillo <josecarrillo8@gmail.com>
2022-08-23 22:03:21 -0600

    Add message

--- hello_controller.js
+++ hello_controller.js
@@ -3,17 +3,17 @@ import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/di
 window.Stimulus = Application.start()
 
 Stimulus.register("hello", class extends Controller {
-    static targets = [ "name", "output" ]
+    static targets = [ "name", "message" ]
 
     connect() {
-        console.log(`Hello, World!!!`) 
+        console.log(this.name)  
     } 
 
-    greet() { 
+    /* greet() { 
         this.outputTarget.textContent = `Hello, ${this.name}!`
-    }
+    } */
     
     get name() {
-        return this.nameTarget.value
+        return this.messageTarget.textContent
     }
 })
