0734fc8934530a619ea6391b7ee48ebb7b31f8af
Jose Carrillo <josecarrillo8@gmail.com>
2022-08-24 16:28:06 -0600

    Add format phones

--- test_controller.js
+++ test_controller.js
@@ -3,11 +3,23 @@ import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/di
 window.Stimulus = Application.start()
 
 Stimulus.register("test", class extends Controller {
-    static targets = [ "message" ]
+    static targets = [ "message", "USAPhone", "CRCPhone" ]
 
     connect() {
-        console.log(this.message)  
+        console.log(this.message)
     } 
+
+    handleUSAPhoneChange() {
+        IMask(this.USAPhoneTarget, {
+            mask: '(000) 000-0000'
+        })
+    }
+
+    handleCRCPhoneChange() {
+        IMask(this.CRCPhoneTarget, {
+            mask: '+(000) 0000-0000'
+        })
+    }
     
     get message() {
         return this.messageTarget.textContent
